90ab1ec3391372bf5ef3c8cf89dc105000cbb2c0
JesusRRr <rolonjesus1996@gmail.com>
2020-07-13 15:35:20 -0500

    chore: create producer properties

--- src/main/java/com/rolon/kafka/tutorial1/ProducerDemo.java
+++ src/main/java/com/rolon/kafka/tutorial1/ProducerDemo.java
@@ -1,8 +1,21 @@
 package com.rolon.kafka.tutorial1;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        String bootstrapServer="127:0.0.1:9092";
+        // Create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        // Create the producer
+
+        // send data
     }
 }
