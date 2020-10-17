77d12eab434242fff5f6e162800b28c2ff6ee69d
JesusRRr <rolonjesus1996@gmail.com>
2020-10-17 14:21:51 -0500

    chore: add new package tutorial2 and add Config.properties to .gitigore

---
+++ src/main/java/com/rolon/kafka/tutorial2/TwiterProducer.java
@@ -0,0 +1,10 @@
+package com.rolon.kafka.tutorial2;
+
+public class TwiterProducer {
+
+    public static void main(String[] args){
+        System.out.println("Hello");
+        String varibale = System.getenv("JAVA_HOME");
+        System.out.println(varibale);
+    }
+}
